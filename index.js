075703ff049adff0ff011a1a02ab5b526bb9ebca
Leo Wu <leowu4ever@users.noreply.github.com>
2019-01-07 12:40:54 +0800

    Now be able to update table rows

--- index.js
+++ index.js
@@ -57,9 +57,7 @@ function getRecords() {
   var user = ($(this).parent().text().replace("View", "")) + "/"
   urlPfx = ""
   urlPfx = user
-
-  console.debug(urlPfx)
-
+  //console.debug(urlPfx)
   var count = 0
   var db = firebase.database()
   db.ref("/" + user).once('value', function(snap) {
@@ -82,10 +80,44 @@ function showDetails() {
 
   var date = ($(this).parent().text().replace("Details", "")) + "/"
   urlPfx = urlPfx + date
-  console.debug(urlPfx)
+  createDetailsTable();
+  updateDetailsTable()
+}
 
+function updateDetailsTable() {
+  updateTableRow("/1_User Info/1_Name", "detail_name")
+  updateTableRow("/1_User Info/2_Start time", "detail_start_time")
+  updateTableRow("/1_User Info/3_Time Trained", "detail_duration_trained")
+
+  updateTableRow("/2_Training Configuration/1_Activity", "detail_activity")
+  updateTableRow("/2_Training Configuration/2_Duration", "detail_duration")
+  updateTableRow("/2_Training Configuration/3_Task", "detail_cognitive_task")
+  updateTableRow("/2_Training Configuration/4_Difficulty level", "detail_task_difficulty")
+
+  updateTableRow("/3_Cognitive Performance/1_Stimulus", "detail_stimulus")
+  updateTableRow("/3_Cognitive Performance/2_Nogo", "detail_nogo")
+  updateTableRow("/3_Cognitive Performance/3_Responses", "detail_responses")
+  updateTableRow("/3_Cognitive Performance/4_Hits", "detail_hits")
+  updateTableRow("/3_Cognitive Performance/5_Lapses", "detail_lapses")
+  updateTableRow("/3_Cognitive Performance/6_Accuracy", "detail_accuracy")
+  updateTableRow("/3_Cognitive Performance/7_Average response time", "detail_avg_response_time")
+
+  updateTableRow("/4_Physical Performance/1_Distance", "detail_distance")
+  updateTableRow("/4_Physical Performance/2_Average speed", "detail_avg_speed")
+  updateTableRow("/4_Physical Performance/3_Average pace", "detail_avg_pace")
+}
+
+function updateTableRow(att, id) {
+  var db = firebase.database()
+  db.ref(urlPfx + att).once('value', function(snap) {
+    document.getElementById(id).innerHTML = snap.val()
+  })
+}
+
+function createDetailsTable () {
   $('#div_users').empty()
   $('#div_users').append(
+
   '<table border="1">' +
     '<tr>' + 
       '<th colspan="3" align="center">General info</th>' +
@@ -98,9 +130,9 @@ function showDetails() {
     '</tr>' + 
 
     '<tr>' +
-      '<td>' + getDetailOf("/1_User Info/1_Name") + '</td>' +
-      '<td>' + getDetailOf("/1_User Info/2_Start time") + '</td>' +
-      '<td>' + getDetailOf("/1_User Info/3_Time Trained") + '</td>' +
+      '<td id = "detail_name"></td>' +
+      '<td id = "detail_start_time"></td>' +
+      '<td id = "detail_duration_trained"></td>' +
     '</tr>' +
 
   '</table border="1">' +
@@ -118,22 +150,23 @@ function showDetails() {
     '</tr>' + 
 
     '<tr>' +
-      '<td>Jill</td>' +
-      '<td>Smith</td>' +
-      '<td>50</td>' +
-      '<td>50</td>' +
+      '<td id = "detail_activity"></td>' +
+      '<td id = "detail_duration"></td>' +
+      '<td id = "detail_cognitive_task"></td>' +
+      '<td id = "detail_task_difficulty"></td>' +
     '</tr>' +
 
   '</table>' +
 
   '<table border="1">' +
     '<tr>' + 
-      '<th colspan="6" align="center">Cognitive performance</th>' +
+      '<th colspan="7" align="center">Cognitive performance</th>' +
     '</tr>' + 
 
     '<tr>' + 
       '<th>Stimulus</th>' +
-      '<th>No-go</th>' + 
+      '<th>No-go</th>' +
+      '<th>Responses</th>' + 
       '<th>Hits</th>' + 
       '<th>Lapses</th>' + 
       '<th>Accuracy</th>' + 
@@ -141,12 +174,13 @@ function showDetails() {
     '</tr>' + 
 
     '<tr>' +
-      '<td>Jill</td>' +
-      '<td>Smith</td>' +
-      '<td>50</td>' +
-      '<td>50</td>' +
-      '<td>50</td>' +
-      '<td>50</td>' +
+      '<td id = "detail_stimulus"></td>' +
+      '<td id = "detail_nogo"></td>' +
+      '<td id = "detail_responses"></td>' +
+      '<td id = "detail_hits"></td>' +
+      '<td id = "detail_lapses"></td>' +
+      '<td id = "detail_accuracy"></td>' +
+      '<td id = "detail_avg_response_time"></td>' +
     '</tr>' +
 
   '</table>' +
@@ -162,19 +196,11 @@ function showDetails() {
     '</tr>' + 
 
     '<tr>' +
-      '<td>Jill</td>' +
-      '<td>Smith</td>' +
-      '<td>50</td>' +
+      '<td id = "detail_distance"></td>' +
+      '<td id = "detail_avg_speed"></td>' +
+      '<td id = "detail_avg_pace"></td>' +
     '</tr>' +
 
   '</table>' 
   )
-}
-
-function getDetailOf(att) {
-  var db = firebase.database()
-  db.ref(urlPfx + att).once('value', function(snap) {
-    console.debug(snap.val())
-    return snap.val()
-  })
 }
